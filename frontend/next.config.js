777e7b1f452b09de85a48432afdc7b8f18f3ad51
YALLA NAGAPRAVEEN <nagapraveenyalla@gmail.com>
2026-01-03 15:41:49 +0530

    updatee

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
     images: {
         unoptimized: true,
     },
     reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://127.0.0.1:8000/:path*',
-            },
-        ]
-    },
 }
 
 module.exports = nextConfig
