0e42f32055dae99cf71d9585bab5e34e8b7d80d4
YALLA NAGAPRAVEEN <nagapraveenyalla@gmail.com>
2026-01-03 15:26:00 +0530

    Restore local API rewrites for development

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
         unoptimized: true,
     },
     reactStrictMode: true,
-    /*
     async rewrites() {
         return [
             {
@@ -14,7 +13,6 @@ const nextConfig = {
             },
         ]
     },
-    */
 }
 
 module.exports = nextConfig
